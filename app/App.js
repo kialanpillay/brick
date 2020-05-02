f16b8bacaffa3abc23d6f0e2df0f85769fb4b510
Kialan Pillay <kialanpillay@gmail.com>
2020-05-02 22:41:01 +0200

    Navigation

--- app/App.js
+++ app/App.js
@@ -1,81 +1,49 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Login from './components/Login.js';
+import Home from './components/Home.js';
+import Search from './components/Search.js';
+import FlashMessage from "react-native-flash-message";
+Stack = createStackNavigator();
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      apiKey: '3-erab-QjV7-Kvhvh',
+      hash: '',
+      status: 'Empty',
+      loading: false,
+    };
+    this.setUser = this.setUser.bind(this);
+  }
 
-function App(){
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Brickset</Text>
-              <Text style={styles.sectionDescription}>
-                The home of Brickset.
-              </Text>
-            </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+  setUser = (username, hash) => {
+    this.setState({username: username, hash: hash});
+  };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
+  render() {
+    return (
+      <>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" options={{headerShown: false}}>
+              {props => <Login {...props} setUser={this.setUser} />}
+            </Stack.Screen>
+            <Stack.Screen name="Home" options={{headerShown: false}}>
+              {props => <Home {...props} username={this.state.username} hash={this.state.hash} />}
+            </Stack.Screen>
+            <Stack.Screen name="Search" options={{headerShown: false}}>
+              {props => <Search {...props} username={this.state.username} hash={this.state.hash} />}
+            </Stack.Screen>
+          </Stack.Navigator>
+          <FlashMessage ref="myLocalFlashMessage" />
+        </NavigationContainer>
+      </>
+    );
+  }
+}
 export default App;
